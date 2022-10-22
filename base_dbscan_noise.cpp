5eb1255d5b1065d6ecdbd4a093bff11a45dff478
Xteom <mateodelaroche@gmail.com>
2022-10-22 04:00:17 -0500

    last commit before parallel imp

--- base_dbscan_noise.cpp
+++ base_dbscan_noise.cpp
@@ -7,8 +7,8 @@
 using namespace std;
 
 
-int distance(int x1, int y1, int x2, int y2)
-{
+float distance(float x1, float y1, float x2, float y2)
+{   
     return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
 
@@ -16,11 +16,53 @@ int distance(int x1, int y1, int x2, int y2)
 //epsilon radio de busqueda, umbral de densidad
 //min_samples numero minimo de puntos para formar un cluster
 //size numero de puntos
+
+// noise_detection que siempre recorre todos los puntos
+// void noise_detection1(float** points, float epsilon, int min_samples, long long int size) {
+//     cout << "Step 0" << "\n"; 
+//     //recorremos todos los puntos
+//     for (long long int i=0; i < size; i++) {
+//         int vecinos = 0;
+//         //medimos la distancia de cada punto con todos los demas
+//         for (long long int j=0; j < size; j++) {
+//             if (distance(points[i][0], points[i][1], points[j][0], points[j][1]) < epsilon) {
+//                 vecinos++;
+//             }
+//         }
+//         if(vecinos-1 < min_samples) {
+//             points[i][2] = 0;
+//         }
+//         else {
+//             points[i][2] = 1;
+//         }
+//     }      
+//     cout << "Complete" << "\n"; 
+// }
+
 void noise_detection(float** points, float epsilon, int min_samples, long long int size) {
     cout << "Step 0" << "\n"; 
+    
+    //recorremos todos los puntos
     for (long long int i=0; i < size; i++) {
+        int vecinos = 0;
+        //medimos la distancia de cada punto con todos los demas
+        for (long long int j=0; j < size; j++) {
+            if(j==i) continue;
+            if (distance(points[i][0], points[i][1], points[j][0], points[j][1]) < epsilon) {
+                vecinos++;
+            }
+            //si ya encontramos el minimo de vecinos, paramos    
+            if (vecinos >= min_samples) {
+                points[i][2] = 1;
+                break;
+            }
+        
+        }
 
-        points[i][2] = rand() % 2;
+        // if(vecinos < min_samples) {
+        //     points[i][2] = 0;
+        // }
+
     }      
     cout << "Complete" << "\n"; 
 }
@@ -58,10 +100,19 @@ int main(int argc, char** argv) {
 
     const float epsilon = 0.03;
     const int min_samples = 10;
-    const long long int size = 4000;
+    const long long int size = 20000;
     const string input_file_name = to_string(size)+"_data.csv";
     const string output_file_name = to_string(size)+"_results.csv";    
     float** points = new float*[size];
+    double start = 0;
+    double end = 0;
+
+    int num_puntos = atoi(argv[1]);
+    int num_hilos = atoi(argv[2]);
+    // puntos 20000, 40000, 80000, 120000, 140000, 160000, 180000, 200000
+    // hilos 1, tot/2, tot, 2tot
+
+    omp_set_num_threads(num_hilos);
 
     for(long long int i = 0; i < size; i++) {
         points[i] = new float[3]{0.0, 0.0, 0.0}; 
@@ -71,10 +122,22 @@ int main(int argc, char** argv) {
     }
 
     load_CSV(input_file_name, points, size);
-    
-    noise_detection(points, epsilon, min_samples, size); 
         
+    start = omp_get_wtime();
+    noise_detection(points, epsilon, min_samples, size); 
+    end = omp_get_wtime();
+
+    //sin if se tarda mucho mas 
+    // double start1 = 0;
+    // double end1 = 0;
+    // start1 = omp_get_wtime();
+    // noise_detection1(points, epsilon, min_samples, size); 
+    // end1 = omp_get_wtime();
+    // cout << "Time sin if: " << end1 - start1 << "\n";
+
     save_to_CSV(output_file_name, points, size);
+
+    cout << "Time con if: " << end - start << "\n";
 
     for(long long int i = 0; i < size; i++) {
         delete[] points[i];
