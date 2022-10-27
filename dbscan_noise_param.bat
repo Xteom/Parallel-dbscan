16663a26514844bf56d1d50232d161b8361c705d
BrunoMol08 <bmolinaz@itam.mx>
2022-10-27 16:52:29 -0500

    commit pre final

--- dbscan_noise_param.bat
+++ dbscan_noise_param.bat
@@ -4,17 +4,17 @@ SET /A p = %1
 ::threads o hilos totales disponibles
 SET /A n = %2
 
-SET /A n_medios = %n_medios%
-SET /A n_dobles = %n_dobles%
+SET /A n_medios = %n% / 2
+SET /A n_dobles = %n% * 2
 
 :: header
 :: echo "datos,modo,hilos,tiempo" >> .\dbscan_noise_res.csv
 
 :: serial
-.\Parallel-dbscan\serial_noise.exe %p% >> .\Parallel-dbscan\dbscan_noise_res2.csv
+.\dbscan_serial_noise.exe %p% >> .\dbscan_noise_res_p.csv
 
 :: parallel puntos 
-.\Parallel-dbscan\dbscan_parallel.exe %p% 1 >> .\Parallel-dbscan\dbscan_noise_res2.csv
-.\Parallel-dbscan\dbscan_parallel.exe %p% %n_medios% >> .\Parallel-dbscan\dbscan_noise_res2.csv
-.\Parallel-dbscan\dbscan_parallel.exe %p% %n% >> .\Parallel-dbscan\dbscan_noise_res2.csv
-.\Parallel-dbscan\dbscan_parallel.exe %p% %n_dobles% >> .\Parallel-dbscan\dbscan_noise_res2.csv
+.\dbscan_parallel_noise.exe %p% 1 >> .\dbscan_noise_res_p.csv
+.\dbscan_parallel_noise.exe %p% %n_medios% >> .\dbscan_noise_res_p.csv
+.\dbscan_parallel_noise.exe %p% %n% >> .\dbscan_noise_res_p.csv
+.\dbscan_parallel_noise.exe %p% %n_dobles% >> .\dbscan_noise_res_p.csv
